3b273c0c1c7f7128fcba713c1f1a8dd1e1394db6
san-rizz-777 <gundesanskar71@gmail.com>
2025-09-20 10:21:22 +0530

    Add binary exponentiation algorithm.

---
+++ binary_expo.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//Implementation of binary exponentation
+
+//Basically algorithm states calcaulating the exponent by squaring 
+
+//Let n be expo then represent it in base 2 it will e floor(logn)+1 digits just have calculate this much operations 
+//Tc- O(logn)
+
+
+//Recursive
+long long binpow(long long a, long long b) {
+    if (b == 0)
+        return 1;
+    long long res = binpow(a, b / 2);
+    if (b % 2)
+        return res * res * a;
+    else
+        return res * res;
+}
+
+
+
+//Bit wise operations
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+//Computing the modulo of large numbers
+long long binpow(long long a, long long b, long long m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
